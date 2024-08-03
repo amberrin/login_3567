0d56846052b01fed2ce9ff04003a99617c99b87a
amberrin <73347974+amberrin@users.noreply.github.com>
2024-08-03 11:07:44 +0300

    Update filevp.py

--- filevp.py
+++ filevp.py
@@ -2,9 +2,19 @@ logins={
     "login"}
 password={"3567"}
 h = 0
-a = input("Введіть логін:")
-i = input("Додати новий логін - s")
-if a in logins:
+a = input("Введіть логін: (Додати новий логін - s)")
+if a == "s":
+    f = input("Введіть новий логін:")
+    logins = [f]
+    o = input("Введіть новий пароль:")
+    password = [o]
+    print("Ваш логін був добавлен у базу даних")
+    x = input("Введіть логін щоб війти")
+    if x in logins:
+        l = input("Введіть пароль")
+        if l in password:
+            print("Ви війшли у систему")
+elif a in logins:
     b = input("Введіть пароль:")
     if b in password:
         print("Ви війшли")
@@ -12,3 +22,15 @@ if a in logins:
         print("Помилка! Пароль введено не вірно")
 else:
     print("Помилка! Такого логіна не існує")
+    v = input("Додати новий логін - s")
+    if v == "s":
+        f = input("Введіть новий логін:")
+        logins = [f]
+        o = input("Введіть новий пароль:")
+        password = [o]
+        print("Ваш логін був добавлен у базу даних")
+        x = input("Введіть логін щоб війти")
+        if x in logins:
+            l = input("Введіть пароль")
+            if l in password:
+                print("Ви війшли у систему")
